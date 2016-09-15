f573ff9e991d106d0de28ef629185b1c3d8d8069
sabroso <zarnatron@gmail.com>
2016-09-15 12:48:45 -0500

    Update fire.py

--- Proyecto Sinergia/fire.py
+++ Proyecto Sinergia/fire.py
@@ -8,8 +8,8 @@ def conectar(x):
 	ruta = 'bd/sga.fdb'
 	con = sg.connect(
 			dsn = ruta,
-			user = 'sysdba',
-			password = 'sabroso',
+			user = 'sysdba',#Usuario por defecto en firebird
+			password = '',# aqui va la contraseña que pusiste al instalar firebird en tu pc
 			charset ='utf8'		
 	)
 
@@ -23,25 +23,4 @@ def conectar(x):
 	cx.close()
 	con.close()
 	return datos
-
-
-
-
-
-
-
-
-
-# a = 'angy'
-# b = 'angy@mail.com'
-# sql5 = ("SELECT * FROM personal WHERE nombre = '%s' and correo = '%s' " % (a,b ))
-# datos = conectar(sql5) 
-
-# # fila = cx.fetchall()
-# print(datos[0][7])
-
-# con.commit()
-# con.close()
-# print('Consulta exitosa !!!!')
-# input('')
-
+#.........................................................................................................
